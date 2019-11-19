35c42e4057b86307d76948532906fa9c6c8ef002
Harold Pratt <hpratt@microsoft.com>
2019-11-19 11:28:21 -0800

    Initial creation of Win32 playground app

---
+++ packages/playground/windows/playground-win32/Playground-Win32.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+#include <windows.h>
+
+int WinMain(HINSTANCE instance, HINSTANCE, PSTR commandLine, int showCmd) {
+  UNREFERENCED_PARAMETER(instance);
+  UNREFERENCED_PARAMETER(instance);
+}
